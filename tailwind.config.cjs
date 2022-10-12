67c47fa8e8ec3181d3ba05e38f7e7ab900accfa4
Bruno Peres <bruno_16515@hotmail.com>
2022-10-12 12:57:37 -0300

    Componentes finalizados

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,30 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.tsx",
-  ],
+  content: ["./src/**/*.tsx"],
   theme: {
-      fontSize: {
-        xs: 14,
-        sm: 16,
-        md: 18,
-        lg: 20,
-        xl: 24,
-        "2xl": 32
-      },
-      colors: {
-        "gray-900": "#121214",
-        "gray-800": "#202024",
-        "gray-400": "#7C7C8A",
-        "gray-200": "#C4C4CC",
-        "gray-100": "#E1E1E6",
+    fontSize: {
+      xs: 14,
+      sm: 16,
+      md: 18,
+      lg: 20,
+      xl: 24,
+      "2xl": 32,
+    },
+    colors: {
+      transparent: "transparent",
+      black: "#000",
+      white: "#FFF",
 
-        "cyan-500": "#81D8F7",
-        "cyan-300": "#9BE1FB",
+      gray: {
+        900: "#121214",
+        800: "#202024",
+        400: "#7C7C8A",
+        200: "#C4C4CC",
+        100: "#E1E1E6",
       },
-      fontFamily: {
-        sans: "Inter, sans-serif",
+
+      cyan: {
+        500: "#81D8F7",
+        300: "#9BE1FB",
       }
+    },
+    fontFamily: {
+      sans: "Inter, sans-serif",
+    },
   },
   plugins: [],
-}
+};
