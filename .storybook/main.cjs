9e1556b058f368fd3ac9dadca21de3df48da031d
Bruno Peres <bruno_16515@hotmail.com>
2022-10-13 10:01:47 -0300

    :tada: Aula 03 Ignite Lab finalizada

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,25 +1,23 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    "@storybook/addon-a11y",
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-vite"
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-vite",
   },
-  "features": {
-    "storyStoreV7": true
+  features: {
+    storyStoreV7: true,
   },
   viteFinal: (config, { configType }) => {
-    if (configType === 'PRODUCTION') {
-      config.base = '/IgniteLab-Design_System/'
+    if (configType === "PRODUCTION") {
+      config.base = "/IgniteLab-Design_System/";
     }
 
-    return config
-  }
-}
+    return config;
+  },
+};
--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -7,9 +7,10 @@ export interface TextProps {
   size?: "sm" | "md" | "lg";
   children: ReactNode;
   asChild?: boolean;
+  className?: string;
 }
 
-export function Text({ size = "md", children, asChild }: TextProps) {
+export function Text({ size = "md", children, asChild, className }: TextProps) {
   const Comp = asChild ? Slot : "span";
 
   return (
@@ -18,7 +19,7 @@ export function Text({ size = "md", children, asChild }: TextProps) {
         "text-xs": size === "sm",
         "text-sm": size === "md",
         "text-md": size === "lg",
-      })}
+      }, className )}
     >
       {children}
     </Comp>
